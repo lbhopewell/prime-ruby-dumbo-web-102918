04ebb745b344e6f7965aea9ed0eb823c086ca49a
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-21 14:06:39 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,12 +2,13 @@
 
 def prime?(number)
   counter = 2
-  
+  if number < 0 
+    return false 
   while counter < number 
    if number % counter ==  0
     false 
   else 
     true
     end
-    
+ end   
 end 
