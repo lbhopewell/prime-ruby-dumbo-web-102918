21e789d61d43759dda1e66a4be014ee74a520f92
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-21 14:11:11 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,15 +2,15 @@
 
 def prime?(number)
   counter = 2
-  if number < 1 
-    return false 
-  else
-  while counter < number 
-   if number % counter ==  0
-    false 
-  else 
-    true
-    end
- end   
+    if number < 1 
+      return false 
+    else
+        while counter < number 
+         if number % counter ==  0
+          false 
+        else 
+          true
+          end
+    end   
 end 
 end
