bcbddc26aa5aeefc17b8bba343dd025b47365045
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-21 15:04:17 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,7 +7,7 @@ def prime?(number)
   
   prime_flag = true
   counter = 2
-  while (counter < number / 2)
+  while (counter < number)
     if (number % counter ==  0)
       prime_flag = false 
     break
