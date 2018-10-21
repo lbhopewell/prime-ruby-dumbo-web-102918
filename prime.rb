513fb18b8be420637bb3ebd537a17785aca6b27f
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-21 15:09:18 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+
+def prime?(number)
+  if number <= 1 
+    return false
+  end  
+  
+  prime_flag = true
+  counter = 2
+  while (counter < number)
+    if (number % counter ==  0)
+      prime_flag = false 
+    break
+    end
+    counter += 1
+end
+return prime_flag
+end
+
